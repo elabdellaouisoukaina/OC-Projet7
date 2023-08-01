e8bdbcc0d8082cbd98a45be32f0c9da33503395e
Soukaïna El Abdellaoui <elabdellaoui.soukaina@gmail.com>
2023-08-01 02:49:38 +0200

    SearchBar - Algorithme de tri (for..) : trouve avec min 3 lettres dans ingrédients, ustensiles et appareils. Combinaison mots clés à faire

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,6 +1,36 @@
 import {recipes} from "../../data/recipes.js"
 import {createCard} from "../utils/recipeCard.js"
+import {search} from "../utils/searchBar.js"
 
 for (let i=0; i < recipes.length; i++) {
     createCard(recipes[i])
 }
+
+
+// Tests
+// console.log(search(recipes, "Thon Rouge (ou blanc)").length);
+// console.log(search(recipes, "Thon").length);
+// console.log(search(recipes, "Casserole").length);
+// console.log(search(recipes, "Casse").length);
+// console.log(search(recipes, "couteau").length);
+// console.log(search(recipes, "économe").length);
+
+const searchInput = document.querySelector('.searchBar');
+
+searchInput.addEventListener("input", (e) => {
+    let value = e.target.value
+
+    // 2. check: if input exists and if input is minimum 3
+    if (value && value.trim().length > 2){
+        document.querySelector('.resultsGallery').innerHTML = "";
+        const resultRecipes = search(recipes, value);
+        
+        for (let i=0; i < resultRecipes.length; i++) {
+            createCard(resultRecipes[i])
+        }
+    } else {
+        // 5. return nothing
+        // input is invalid -- show an error message or show no results
+    }
+
+});
---
+++ scripts/utils/searchBar.js
@@ -0,0 +1,38 @@
+export function search(recipes, value){ // entry est un mot complet ou au minimum 3 lettres
+    const results = [];
+    let entry = value.trim().toLowerCase();
+
+    //Parcours tous les recettes données
+    for (let index = 0; index < recipes.length; index ++){
+
+        // Cherche entry dans tous les ingrédients de la recette
+        const ingredients = recipes[index].ingredients;
+
+        for (let i = 0; i < ingredients.length; i ++){
+            let ingredient = ingredients[i].ingredient.trim().toLowerCase();
+            if (ingredient.includes(entry)){
+                results.push(recipes[index]);
+            }
+        }
+
+        // Vérifie si l'appareil de la recette correspond à entry
+        const appliance = recipes[index].appliance.trim().toLowerCase();
+        if (appliance.includes(entry)){
+            results.push(recipes[index])
+        }
+
+        // Cherche entry dans tous les ustensiles de la recette
+        const ustensils = recipes[index].ustensils;
+
+        for (let i = 0; i < ustensils.length; i ++){
+            let ustensil = ustensils[i].trim().toLowerCase();
+
+            if (ustensil.includes(entry)){
+                results.push(recipes[index]);
+            }
+        }
+    }
+
+    return results; // Retourne une liste des recettes correspondant à la recherche, peut être une liste vide
+}
+
